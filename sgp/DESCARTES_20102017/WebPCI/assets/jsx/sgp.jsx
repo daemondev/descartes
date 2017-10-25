b943842ddaa09763fa5ca827565e5f5a95ef78fd
daemondev <granlinux@gmail.com>
2017-10-25 18:07:25 -0500

    first interaction

--- sgp/DESCARTES_20102017/WebPCI/assets/jsx/sgp.jsx
+++ sgp/DESCARTES_20102017/WebPCI/assets/jsx/sgp.jsx
@@ -1,1 +1,17 @@
 ﻿//My Components for SGP Project
+var ProcessDiv = React.createClass({
+    getInitialState: function () {
+        return { myProps: {}, data: [] };
+    },
+    handleChange: function (e) {
+        this.setState({data:e.target.value});
+    },
+    render: function () {
+
+    }
+});
+
+ReactDOM.render(
+    <ProcessDiv />,
+    document.getElementById('divSGP')
+);
